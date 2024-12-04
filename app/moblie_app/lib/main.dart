343ad3f88e73665d964e1875e64287637534581c
devdino <42527332+lskyub@users.noreply.github.com>
2024-12-04 15:33:52 +0900

    리스트 작업 중

--- app/moblie_app/lib/main.dart
+++ app/moblie_app/lib/main.dart
@@ -70,14 +70,15 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
     });
   }
 
-  void test() async {
-    var result = await ref.read(mainViewModelProvider.notifier).fetchUser();
-    print("${result.statusCode} : ${result.model}");
+  void callApi() {
+    ref.read(mainViewModelProvider.notifier).fetchUser();
   }
 
   @override
   Widget build(BuildContext context) {
-    test();
+    var list = ref.read(mainViewModelProvider.notifier).list;
+    print("${list.length} > ${list}");
+    callApi();
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -94,40 +95,11 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return Text("${list[index].id}", style: TextStyle(color: Colors.black),);
+          }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
