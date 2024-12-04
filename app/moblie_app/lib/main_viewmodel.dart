32250d9ff0a7bc1df8bd03749be745872c73f8df
devdino <42527332+lskyub@users.noreply.github.com>
2024-12-04 16:06:13 +0900

    리스트 적용

--- app/moblie_app/lib/main_viewmodel.dart
+++ app/moblie_app/lib/main_viewmodel.dart
@@ -3,22 +3,21 @@ import 'package:domain/usecase/image_usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:moblie_app/providers.dart';
 
-class MainViewModel extends StateNotifier<String> {
+class MainViewModel extends StateNotifier<List<ImageModel>> {
   final GetImageList getImageList;
 
-  MainViewModel(this.getImageList) : super("");
-  List<ImageModel> list = [];
+  MainViewModel(this.getImageList) : super([]);
 
   void fetchUser() async {
     var response = await getImageList.call();
     if (response.statusCode == 200) {
-      list = response.model;
+      state = response.model;
     }
   }
 }
 
 // ViewModel Provider
 final mainViewModelProvider =
-    StateNotifierProvider<MainViewModel, String>((ref) {
+    StateNotifierProvider<MainViewModel, List<ImageModel>>((ref) {
   return MainViewModel(ref.watch(getImageList));
 });
