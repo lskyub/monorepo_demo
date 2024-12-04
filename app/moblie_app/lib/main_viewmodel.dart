3dcd08fb289942ff65cac732409caae20050437b
devdino <42527332+lskyub@users.noreply.github.com>
2024-12-04 15:33:52 +0900

    리스트 작업 중

--- app/moblie_app/lib/main_viewmodel.dart
+++ app/moblie_app/lib/main_viewmodel.dart
@@ -1,15 +1,19 @@
+import 'package:domain/entity/image_model.dart';
 import 'package:domain/usecase/image_usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:moblie_app/providers.dart';import 'package:domain/entity/image_model.dart';
-import 'package:domain/entity/response.dart';
+import 'package:moblie_app/providers.dart';
 
 class MainViewModel extends StateNotifier<String> {
   final GetImageList getImageList;
 
   MainViewModel(this.getImageList) : super("");
+  List<ImageModel> list = [];
 
-  Future<Response<List<ImageModel>>> fetchUser() async {
-    return await getImageList.call();
+  void fetchUser() async {
+    var response = await getImageList.call();
+    if (response.statusCode == 200) {
+      list = response.model;
+    }
   }
 }
 
